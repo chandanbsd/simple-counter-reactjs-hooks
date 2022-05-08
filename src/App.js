e094cc62077b7e4a1d79a4f9f053edb25eab6bf5
chandanbsd <49138440+chandanbsd@users.noreply.github.com>
2022-05-08 01:27:33 -0400

    UI Components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1>Counter Application</h1>
+      <h3>Counter Value = </h3>
+      <button>Increment Counter</button>
+      <button>Decrement Counter</button>
+      <button>Reset Counter</button>
+    </div>
+  );
 };
 
 export default App;
